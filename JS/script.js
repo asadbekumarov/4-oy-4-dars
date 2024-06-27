6c64c11a7211a9577d30a4d16fbe094ea96b4893
asadbekumarov <asadbekumarov922@gmail.com>
2024-06-28 03:37:32 +0500

    dars

---
+++ JS/script.js
@@ -0,0 +1,211 @@
+// 1-PROBLEM
+
+// const number = prompt("Bu yerga son kiritring men sizga musbat yoki manfiy ekanligini aniqlab beraman.")
+// if (number > 0 ) {
+//     alert("kiritgan soningiz musbat son")
+// console.log(true);
+// }else{
+//     alert("kiritgan soningiz manfiy son")
+// console.console.log(false);
+// }
+
+// 2-PROBLEM
+
+// const number = prompt (`Bu yerga son kiritring men sizga toq yoki toq emasligini aniqlab beraman.`)
+// if (number % 2 ) {
+//     alert("toq son")
+//     console.log(true);
+// }else{
+//     alert("toq son emas")
+//     console.log(false);
+// }
+
+// 3-PROBLEM
+
+// const number = prompt (`Bu yerga son kiritring men sizga juft yoki juft emasligini aniqlab beraman.`)
+// if ((number % 2) ==0 ) {
+//     alert("juft son")
+//     console.log(true);
+// }else{
+//     alert("juft son emas")
+//     console.log(false);
+// }
+
+// 4-PROBLEM
+
+// const number1 =prompt (`Son kiriting`)
+// const number2 =prompt (`Yana son kiriting`)
+// if (number1 > 2 & number2 <= 3) {
+//     alert(`to'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+//     console.log(false);
+// }
+
+// 5-PROBLEM
+
+// const number1 =prompt (`Son kirting`)
+// const number2 =prompt (`yana son kirting`)
+// if (number1 >= 0 || number2 <= -2 ) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+// }console.log(false);
+
+// 6-PROBLEM
+
+// const number1 =prompt (`Birinchi sonni kirting.`)
+// const number2 =prompt (`Ikkinchi sonni kiriting.`)
+// const number3 =prompt (`Oxigi sonni kiriting.`)
+// if (number1 <= number2 && number2 <= number3 ) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+// }console.log(false);
+
+// 7-PROBLEM
+
+// const number1 = prompt(`son kiriting`);
+// const number2 = prompt(`yana son kiriting`);
+// const number3 = prompt(`oxirgi soni kiriting`);
+// if (number1 < number2 && number2 < number3  ||  number3 < number2 && number2 < number1) {
+//     alert(`To'g'ri`)
+// console.log(true);
+// }else{
+//     alert(`Xato`)
+// console.log(false);
+// }
+
+// 8-PROBLEM
+
+// const number1=prompt(`son kiritng`)
+// const number2=prompt(`yan son kiritng`)
+// if (number1 % 2 && number2 % 2) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+// }console.log(false);
+
+// 9-PROBLEM
+
+// const number1=prompt(`son kiritng`)
+// const number2=prompt(`yan son kiritng`)
+// if (number1 % 2 || number2 % 2) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+// }console.log(false);
+
+// 10-PROBLEM
+
+// const number1=prompt(`son kiritng`)
+// const number2=prompt(`yan son kiritng`)
+// if (number1 % 2 !==0 || number2 % 2 !==0) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+// }console.log(false);
+
+// 11-PROBLEM
+
+// const number1=prompt (`Son kiritng`)
+// const number2=prompt(`Yana son kiritng`)
+// if (number1 % 2 ===0 && number2 % 2 ===0||number1 % 2 !==0 && number2 % 2 !==0) {
+//     alert(`To'g'ri`)
+//     console.log(true);
+// }else{
+//     alert(`Xato`)
+//     console.log(false)
+// }
+
+// 12-PROBLEM
+
+// const number1 = prompt(`son kiriting`);
+// const number2 = prompt(`yana son kiriting`);
+// const number3 = prompt(`oxirgi soni kiriting`);
+// if (number1 > 0 && number2 > 0 && number3 > 0) {
+//   alert(`To'g'ri`);
+//   console.log(true);
+// } else {
+//   alert(`Xato`);
+//   console.log(false);
+// }
+
+// 13-PROBLEM
+
+// const number1 = prompt(`son kiriting`);
+// const number2 = prompt(`yana son kiriting`);
+// const number3 = prompt(`oxirgi soni kiriting`);
+// if (number1 > 0 || number2 > 0 || number3 > 0) {
+//   alert(`To'g'ri`);
+//   console.log(true);
+// } else {
+//   alert(`Xato`);
+//   console.log(false);
+// }
+
+// 14-PROBLEM
+
+// const number1 = prompt(`son kiriting`);
+// const number2 = prompt(`yana son kiriting`);
+// const number3 = prompt(`oxirgi soni kiriting`);
+// if (number1 > 0 || number2 > 0  || number2 > 0 || number3 > 0) {
+//   alert(`To'g'ri`);
+//   console.log(true);
+// } else {
+//   alert(`Xato`);
+//   console.log(false);
+// }
+
+// 15-PROBLEM
+
+// const number1 = prompt(`son kiriting`);
+// const number2 = prompt(`yana son kiriting`);
+// const number3 = prompt(`oxirgi soni kiriting`);
+// if (number1 > 0 & number2 > 0 & number3 > 0) {
+//     alert(`To'g'ri`)
+//     console.log(true)
+// }else{
+//     alert(`Xato`)
+//     console.log(false);
+// }
+
+// 16-PROBLEM
+
+// const number1=prompt(`son kirting`)
+// // const number2=prompt(`yana son kiriting`)
+// if ( number1 > 9 && number1 <= 99 && number1 % 2 === 0 ) {
+//     alert(`To'g'ri`)
+//     console.log(true)
+// }else{
+//     alert(`Xato`)
+//     console.log(false)
+// }
+
+// QO'SHIMCHA
+
+// 1-PROBLEM
+
+// let num = +prompt("son kirting");
+// if (num > 0) {
+//   alert(num += 1)
+// } else if (num < 0) {
+//   alert(num -= 2)
+// } else {
+//     alert('0 son emas')
+// }
+
+
+// DARK-MODE
+
+// let btn =document.querySelector('button')
+// btn.addEventListener('click', ()=>{
+// document.body.classList.add('Tun')
+// document.body.classList.toggle('tun')
+// });
